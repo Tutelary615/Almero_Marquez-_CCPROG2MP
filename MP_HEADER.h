758d5c66e6205f739349b5b41d9478fee3b8b806
Tutelary615 <marqueztj34@gmail.com>
2025-03-08 15:01:04 +0800

    added function prototypes

--- MP_HEADER.h
+++ MP_HEADER.h
@@ -14,3 +14,18 @@ typedef struct {
 	languageTranslationPair pairs[10];
 	int pairCount;
 } entry;
+
+bool getInteger(int* num);
+void printMenu(string50 choices[], int numberOfChoices);
+bool isMenuInputValid(bool isInputValid, int lowerBound, int upperBound, int input);
+int getAndValidateMenuInput(int lowerBound, int upperBound);
+int mainMenu();
+int manageDataMenu(int entryCount);
+void getPair(string20 tempLanguage, string20 tempTranslation, 
+             char* characterAfterLanguage, char* characterAfterTranslation);
+bool isLanguageTranslationPairValid(string20 tempLanguage, string20 tempTranslation, 
+                                    char characterAfterLanguage, char characterAfterTranslation);
+void formatLanguageOrTranslation(string20 str);
+void addEntry(entry* e);
+void sortEntryAlphabeticallyByLanguage(entry* e);
+int searchForLanguageTranslationPair(entry e, string20 languageKey, string20 translationKey);
