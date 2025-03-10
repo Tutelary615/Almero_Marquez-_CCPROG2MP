b215a6b6f0d3d633bb0d9ad9eb18d7c9203fbb69
Tutelary615 <marqueztj34@gmail.com>
2025-03-10 13:58:20 +0800

    defined MAXPAIRSPERENTRY

--- MP_HEADER.h
+++ MP_HEADER.h
@@ -1,5 +1,6 @@
 #define ERRORFORMATSTRING "\033[0;31m%s\033[0m"
 #define WARNINGFORMATSTRING "\033[0;33m%s\033[0m"
+#define MAXPAIRSPERENTRY 10
 
 typedef char string20[21];
 typedef char string50[51];
