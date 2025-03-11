9da4129da3c72f31284a0611b7a3aa49e90a5c96
Tutelary615 <marqueztj34@gmail.com>
2025-03-11 23:19:12 +0800

    renamed ERRORMESSAGEFORMATSTRING to REDFORMATSTRING

--- MP_HEADER.h
+++ MP_HEADER.h
@@ -1,4 +1,4 @@
-#define ERRORFORMATSTRING "\033[0;31m%s\033[0m"
+#define REDFORMATSTRING "\033[0;31m%s\033[0m"
 #define YELLOWFORMATSTRING "\033[0;33m%s\033[0m"
 #define GREENFORMATSTRING "\033[0;32m%s\033[0m"
 #define PURPLEFORMATSTRING "\033[0;35m%s\033[0m"
@@ -6,6 +6,7 @@
 #define MAXPAIRSPERENTRY 10
 
 typedef char string20[21];
+typedef char string30[31];
 typedef char string50[51];
 
 typedef struct {
